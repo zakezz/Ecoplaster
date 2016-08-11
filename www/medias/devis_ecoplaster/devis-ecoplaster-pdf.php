d8ad7436368880738069b691f4f398fc58a92608
zakezz <zakaria_zz1@hotmail.com>
2016-08-12 00:00:47 +0200

    Déplacement du dossier ecoplaster_devis dans media

---
+++ www/medias/devis_ecoplaster/devis-ecoplaster-pdf.php
@@ -0,0 +1,260 @@
+
+
+<?php
+
+//Connexion à la BDD
+include("bdd-ecoplaster.php");
+
+
+// script permettant la mis en transparence de n'importe quel objet http://www.fpdf.org/en/script/script74.php
+require('alphapdf.php');
+
+$PDF=new AlphaPDF();
+$PDF->AddPage();
+ 
+ 
+ // Page header
+
+    // Logo
+    $PDF->Image('images/ecoplaster_top.png',35,20,120);
+    $PDF->Image('images/icon_Qualibat.png',165,12,20);
+    
+    // On rend l'alpha semi transparent
+	$PDF->SetAlpha(0.5);
+	// Devis provisoire
+    $PDF->Image('images/filigrane_devisprovisoire.jpg',40,100,150);
+
+	// On rend l'alpha opaque
+	$PDF->SetAlpha(0.75);
+    
+    // Position du curseur
+    $PDF->SetXY(15,50);
+    
+    // Arial 
+    $PDF->SetFont('Arial','',10);
+    
+    // entete
+$PDF->MultiCell(30,4,utf8_decode("Date du devis
+Numéro de client
+Emis par
+Validité du devis"), 0,'R');
+    
+$date = date('d/m/Y');    
+$numclient =$donnees['MAX(id_client)'];
+    
+    // Position du curseur
+$PDF->SetXY(70,50);
+    // entete
+$PDF->MultiCell(30,4,"$date
+$numclient
+ECOPLASTER
+90 jours", 0,'R');
+
+$status=$_POST['status'].".";
+$nom=$_POST['nom-prenom'];
+$adresse=$_POST['adresse'];;
+$ville=$_POST['ville'];;
+$cp=$_POST['CP'];
+//Destinataire
+$PDF->SetXY(135,40);
+$PDF->SetFont('Arial','B',10);
+$PDF->MultiCell(0,4,"Destinataire :", 0,'L');
+$PDF->SetXY(135,45);
+$PDF->SetFont('Arial','',10);
+$PDF->MultiCell(30,4,utf8_decode("$status $nom
+$adresse
+$ville
+$cp
+France"), 0,'L');
+
+
+// Information
+$PDF->SetXY(10,75);
+$PDF->SetFont('Arial','B',10);
+$PDF->MultiCell(0,4,"Informations                                                                                                       Contact", 0,'L');
+
+$PDF->SetXY(10,80);
+$PDF->SetFont('Arial','',10);
+$PDF->MultiCell(0,4,utf8_decode("Merci d'avoir choisi Ecoplaster pour nos services.
+Service Après Vente : Garantie 10 ans."), 0,'L');
+
+$PDF->SetXY(132,80);
+$PDF->SetFont('Arial','',10);
+$PDF->MultiCell(0,4,utf8_decode("Adéla KISTAS, Commerciale France
+Tel: 09 52 45 54 60
+email : ak@ecoplaster.fr"), 0,'L');
+    
+    // Line break
+    $PDF->Ln(30);
+
+// Définition des propriétés du tableau.
+$proprietesTableau = array(
+	'TB_ALIGN' => 'L',
+	'L_MARGIN' => 3,
+	'BRD_COLOR' => array(0,0,0),
+	'BRD_SIZE' => '0.3',
+	);
+
+// Définition des propriétés du header du tableau.	
+$proprieteHeader = array(
+	'T_COLOR' => array(0,0,0),
+	'T_SIZE' => 10,
+	'T_FONT' => 'Arial',
+	'T_ALIGN' => 'C',
+	'V_ALIGN' => 'T',
+	'T_TYPE' => 'B',
+	'LN_SIZE' => 5,
+	'BG_COLOR_COL0' => array(215, 228, 188),
+	'BG_COLOR' => array(215, 228, 188),
+	'BRD_COLOR' => array(0,0,0),
+	'BRD_SIZE' => 0.1,
+	'BRD_TYPE' => '1',
+	'BRD_TYPE_NEW_PAGE' => '',
+	);
+
+// Contenu du header du tableau.	
+$contenuHeader = array(
+	100, 20, 15, 30, 20,
+	"[B]Description",utf8_decode("[BC]Quantité"), utf8_decode("[BC]Unité"), "[BC]Prix unitaire HT", "[BC]Total HT",
+	);
+
+// Définition des propriétés du reste du contenu du tableau.	
+$proprieteContenu = array(
+	'T_COLOR' => array(0,0,0),
+	'T_SIZE' => 9,
+	'T_FONT' => 'helvetica',
+	'T_ALIGN_COL0' => 'L',
+	'T_ALIGN' => 'C',
+	'V_ALIGN' => 'M',
+	'T_TYPE' => '',
+	'LN_SIZE' => 3,
+	'BG_COLOR_COL0' => array(255,255,255),
+	'BG_COLOR' => array(255,255,255),
+	'BRD_COLOR' => array(0,0,0),
+	'BRD_SIZE' => 0.1,
+	'BRD_TYPE' => 'L,R',
+	'BRD_TYPE_NEW_PAGE' => '',
+	);	
+
+//recuperation des données
+$surface=$_POST['Surface_totale'];
+$echaffaudage=10;
+$totalITE = 95*$surface;
+$totalEchaffaudage = 9*$echaffaudage;
+$seuil=$_POST['NombreFenetre'];
+$totalSeuil = 115*$seuil;
+$Totalht=$totalSeuil+$totalEchaffaudage+$totalITE;
+$TVA=$Totalht*0.055;
+$virgule=round(fmod($TVA,1) * 1000);
+$TVA=$TVA-(0.001*$virgule);
+$TotalTTC=$TVA+$Totalht;
+
+//sigle €
+$euro = chr(128);
+
+// Contenu du tableau.	
+$contenuTableau = array(
+	utf8_decode("
+	Complexe ITE complet:
+- Mise en place d'une solution ITE (Isolation
+Thermique Extérieure)sur l'ensemble de la
+surface reprise dans ce poste.
+- Finition contemporaine avec grain fin : 1,5 mm
+- Détails : sur isolant 14cm en R = 4,3 (résistance thermique)"), "[C]".$surface , utf8_decode("[C]m²"), "[C]95,00 $euro", "[C]".$totalITE.",00 $euro",
+
+"","","","","",
+"","","","","",
+
+
+utf8_decode("
+Installation échafaudage:
+-Mise en place d'un échafaudage tubulaire d'accès
+conforme aux normes R.G.P.T. sur la surface
+de travail le nécessitant (surface tenant
+compte des facteurs tels que les débordements
+pour raccords, montages de passerelles etc...).
+- Pour les deux pignons."), "[C]".$echaffaudage, utf8_decode("[C]m²"), "[C]9,00 $euro", "[C]".$totalEchaffaudage.",00 $euro",
+
+"","","","","",
+"","","","","",
+
+
+utf8_decode("
+Fourniture et pose de seuil en aluminium
+teinté dans le RAL de votre choix."), "[C]".$seuil, utf8_decode("[C]pièce"), "[C]115,00 $euro", "[C]".$totalSeuil.",00 $euro",
+"","","","","",
+"","","","","",
+"","","","","",
+
+	);	
+ 
+// D'abord le PDF, puis les propriétés globales du tableau. 
+// Ensuite, le header du tableau (propriétés et données) puis le contenu (propriétés et données)
+$PDF->drawTableau($PDF, $proprietesTableau, $proprieteHeader, $contenuHeader, $proprieteContenu, $contenuTableau);
+
+// Position du curseur
+    $PDF->SetXY(133,210);
+// Arial 
+	$PDF->SetFont('Arial','B',9);
+// Total
+	$PDF->MultiCell(0,4,"Total HT
+TVA (5,5%)", 'B','L');
+
+
+$PDF->SetFont('Arial','',9);
+$PDF->SetXY(163,210);
+$PDF->MultiCell(0,4,"$Totalht,00 $euro
+$TVA".","."$virgule $euro", 'B','R');  
+
+
+$PDF->SetFillColor(204, 204, 204);
+$PDF->SetXY(133,220);
+$PDF->SetFont('Arial','B',9);
+$PDF->MultiCell(0,4,"Total TTC", 0,'L', true);  
+$PDF->SetXY(163,220);
+$PDF->SetFont('Arial','B',9);
+$PDF->MultiCell(0,4,$TotalTTC.",".$virgule." $euro", 0,'R', true);  
+
+
+// On rend l'alpha opaque
+	$PDF->SetAlpha(1);
+
+// Page footer
+    // détail bancaire
+    $PDF->Image('images/ecoplaster_bottom_fr.png',20,250,170);
+
+
+// enregistre le document dans le répertoire local du serveur.    
+$PDF->Output("devis_clients/".$numclient."_devis_ecoplaster.pdf",'F');
+
+// envoie du mail au client
+include("Mailer-ecoplaster.php");
+
+
+?>
+<!DOCTYPE html">
+<html lang="fr">
+<meta charset="utf-8" />
+	
+<?php
+echo '
+	<iframe id="pagepdf" src="" width="100%" height="100%">
+	[Your browser does <em>not</em> support <code>iframe</code>,
+	or has been configured not to display inline frames.
+	You can access <a href="./test.PDF">the document</a>
+	via a link though.]</iframe>
+';
+
+
+// On ajoute un nouveau client dans client_ecoplaster
+
+$bdd->exec('UPDATE Clients_eco SET lien_devis= \'devis_clients/'.$numclient.'_devis_ecoplaster.pdf\'  WHERE id_client = \''.$numclient.'\'');
+$req->closeCursor();
+?>
+
+<script>
+	/*le < ? = $ variablephp ? >  permet d'introduire la valeur de la variable*/
+    document.getElementById("pagepdf").src ="devis_clients/"+<?=$numclient;?>+"_devis_ecoplaster.pdf";
+</script>
+
+</html>
---
+++ www/medias/devis_ecoplaster/js/eco.js
@@ -0,0 +1,122 @@
+
+/*Remplace les nombre à virgule entre par des points*/
+function virgule(string){
+		if(string !='')
+		{
+			return string.replace(',', '.');
+		}	
+	return string;
+}
+
+/*cache l'élément*/
+function hide(id)  {
+	document.getElementById(id).style.display='none';
+}
+
+/*affiche l'élément*/
+function show(id){
+	document.getElementById(id).style.display='table-row';
+}
+
+/*affichage des façades*/
+function menufacade(){ 
+	var nbrFacade = $('#Nombre_de_facades').val();
+	
+	for(i=1;i<=nbrFacade;i++){
+		var facadeId ='facade'+i;
+		$('#'+facadeId).show();
+	}
+	for(i=8;i>nbrFacade;i--){
+		var facadeId ='facade'+i;
+		$('#'+facadeId).hide();
+	}
+}
+
+/*Calcul des surfaces totales*/
+function calculSurface() {
+	var total = 0;
+	// taille fenetre 1m² porte 2m² pfenetre 4m² pgarage 10m²
+	var NbrFenetre = $("#NombreFenetre").val()*1;
+	var NbrPorte = $("#NombrePorte").val()*2;
+	var NbrPFenetre = $("#NombrePFenetre").val()*4;
+	var NbrPgarage = $("#NombrePgarage").val()*10;
+	var Surface_Totale =0;
+	var Surface_a_reduire =Math.round((NbrFenetre + NbrPorte + NbrPFenetre + NbrPgarage));
+	var Prixm2 = 97;
+	
+	
+	for(i=1;i<=8;i++){
+		var totalSurface = 0;
+		var pignonId ='facade_avec_pignon_'+i;
+		var largeurId ='Largeur_facade_'+i;
+		var hauteurId ='Hauteur_facade_'+i;
+		var hauteurPignonId ='Hauteur_pignon_'+i;
+		var surfaceTotal ='surface_facade_'+i;
+		
+		if($('input[type=radio][name='+pignonId+']:checked').val()=="oui"){
+			
+			if(virgule($("#"+largeurId).val()) != 0 && virgule($("#"+hauteurId).val()) != 0){
+				var hauteurPignon = virgule($("#"+hauteurPignonId).val())*1;
+				var largeur = virgule($("#"+largeurId).val())*1;
+				var hauteur = virgule($("#"+hauteurId).val())*1;
+				totalSurface = largeur * ((hauteur+hauteurPignon)/2);
+				}
+				
+		}else{
+			if(virgule($("#"+largeurId).val()) != 0 && virgule($("#"+hauteurId).val()) != 0){
+					var largeur = virgule($("#"+largeurId).val())*1;
+					var hauteur = virgule($("#"+hauteurId).val())*1;
+					totalSurface = largeur * hauteur;
+			}
+		}
+		total = Math.round((total + totalSurface));
+	}
+	
+	
+	Surface_Totale = Math.round((total - Surface_a_reduire));
+	
+	$("#Surface_totale").val(Surface_Totale);
+	
+	$("#Prix_totale").val(Surface_Totale* Prixm2 + (Surface_Totale* Prixm2*0.055));
+	
+	$("#Aide").val(Math.round(Surface_Totale* Prixm2 * 0.3));
+	
+	
+	$("#zonesurfacetotale").show("blind", 500);	
+	
+}
+
+		
+/*vérification d'une adresse email valide*/
+function checkEmail(emailVal) {
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if (!filter.test(emailVal)) {
+		alert('Votre adresse email est invalide');
+		return false;
+	} else {
+		return true;
+	}
+}
+
+/* 
+$(function(){
+	$('#images').MultiFile({
+		accept:'gif|jpg|GIF|JPG|jpeg|png|PNG|PDF|pdf|JPEG', max:4, STRING:{
+			remove:'Supprimer',
+			selected:'Selectionnez: $file',
+			denied:'Le format est invalide $ext!',
+			duplicate:'Vous avez dèjà uploader cette image:\n$file!'
+			}
+	});
+	});*/
+
+/*button recevoir_mail*/
+function myFunction() {
+    alert("Merci d'avoir choisis nos service. \n un mail avec votre pré-devis vous a été envoyer.\n par sécurité vérifier vos spam.\n");
+}
+
+
+
+
+
+
